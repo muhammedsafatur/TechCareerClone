4bf66abd982acc07b2f19c231897327bd3a4ff37
muhammedsafatur <msafatur@outlook.com>
2024-11-13 23:03:58 +0300

    Navbar.js color changed and buttons added

--- techcareer-demo-project-boilerplate-main/src/components/Navbar.js
+++ techcareer-demo-project-boilerplate-main/src/components/Navbar.js
@@ -6,7 +6,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import { Button } from '@mui/material';
 import Stack from '@mui/material/Stack';
@@ -46,43 +45,40 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 export default function PrimarySearchAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}> {/* Logo ve başlık için box */}
-            <a href="../Pages/Home">
-              <img src={logo} alt="logo" width="100px" height="25px" />
-            </a>
-            <Typography variant="h6" noWrap component="div" sx={{ ml: 2 }}>
-              Başlık
-            </Typography>
-          </Box>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-          <Box sx={{ flexGrow: 1 }} />
-          <Stack spacing={2} direction="row" sx={{ mr: 2 }}> {/* Butonları doğrudan Stack içine aldık */}
-            <Button variant="text" color="inherit">Buton 1</Button>
-            <Button variant="text" color="inherit">Buton 2</Button>
-            <Button variant="text" color="inherit">Buton 3</Button>
-          </Stack>
-        </Toolbar>
-      </AppBar>
-    </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{ backgroundColor: 'white' }}>
+          <Toolbar>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <a href="../Pages/Home">
+                <img src={logo} alt="logo" width="200px" height="25px" />
+              </a>
+            </Box>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+            <Box sx={{ flexGrow: 1 }} />
+            <Stack spacing={2} direction="row" sx={{ mr: 2 }}>
+              <Button variant="text" sx={{ color: 'black' }}>Buton 1</Button>
+              <Button variant="text" sx={{ color: 'black' }}>Buton 2</Button>
+              <Button variant="text" sx={{ color: 'black' }}>Buton 3</Button>
+            </Stack>
+          </Toolbar>
+        </AppBar>
+      </Box>
   );
 }
