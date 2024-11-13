97d10378c07dbb49a5a954bc5a5d7d4b4cd9cd5c
muhammedsafatur <msafatur@outlook.com>
2024-11-13 19:44:32 +0300

    Carousel.js , EventsSlider.js and Navbar.js created.

---
+++ techcareer-demo-project-boilerplate-main/src/components/EventsSlider.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function EventsSlider() {
+    return ( <div>
+
+
+        </div>
+
+    )
+}
+
+export default EventsSlider
---
+++ techcareer-demo-project-boilerplate-main/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>Footer</div>
+    )
+}
+
+export default Navbar
